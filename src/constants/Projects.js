6788ed4ca9220141083b6ccf3a8a0b4bfd5c9286
Kyle PC <kyle.dahl201@gmail.com>
2020-09-22 13:21:12 -0500

    deploy edit

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -1,15 +1,30 @@
 export const projects = {
-    heading: "Projects",
-    // quote: "''Imagination is more important than knowledge.''",
-    projectList: [
-        {
-            name: "Lunch and Wag",
-            description: "An app designed to help you find dog friendly restaurant patios near you, so you can have a bite with your pup!",
-            link: "https://kyledahl201.github.io/lunch-and-wag/"
-        }, {
-            name: "Drum Kit",
-            description: "JavaScript app to simulate the sounds of a drum kit.  Rock on!",
-            link: "https://kyledahl201.github.io/JS-drum-kit/"
-        }
-    ]
-}
+  heading: "Projects",
+  // quote: "''Imagination is more important than knowledge.''",
+  projectList: [
+    {
+      name: "Lunch and Wag",
+      description:
+        "An app designed to help you find dog friendly restaurant patios near you, so you can have a bite with your pup!",
+      link: "https://kyledahl201.github.io/lunch-and-wag/",
+    },
+    {
+        name: "Bingeworthy",
+        description:
+          "When stuck at home in quarantine, what else to do other than binge watch?  Bingeworthy is an app built with Node, MySQL, and JavaScript designed to help users find 'bingeworthy' shows to watch through clever APIs.",
+        link: "https://production-binge-worthy.herokuapp.com/",
+        
+      },
+    {
+      name: "React Pokedex",
+      description: "Fun pokedex built with React",
+      link: "https://kyledahl201.github.io/react-pokedex/",
+    },
+    {
+      name: "Drum Kit",
+      description:
+        "JavaScript app to simulate the sounds of a drum kit.  Rock on!",
+      link: "https://kyledahl201.github.io/JS-drum-kit/",
+    },
+  ],
+};
