3fce820918bdebf528253e68225b48690ad569e9
Kyle PC <kyle.dahl201@gmail.com>
2022-04-15 12:07:56 -0500

    edit about me section

--- src/constants/About.js
+++ src/constants/About.js
@@ -1,6 +1,6 @@
 export const about = {
     heading: "About Me",
     // "quote": "''Pick a flower on Earth and you move the farthest star.''",
-    "bio": "After years as an executive chef in some of North America's top restaurants, Kyle has focused his passions into what he truly cares most about: engineering technology that can better the world.",
-    "bio2": "Based in Austin and a graduate from the University of Texas bootcamp for full-stack web development and software engineering, Chef Kyle is a focused and quick-learning software engineer and developer. Experienced in both front end and back end construction, computer science, programming, and UX design for a multitude of projects. An incredible chef, but even better at JavaScript. "
+    "bio": "Hi there!  My name is Kyle and I love to create things and solve problems.  After years of being an executive chef in top restaurants across the United States, I have left the restaurant industry behind for a new found passion.  Computer science is so fascinating to me, and as a developer I love to employ the same skills used as a chef to adapt to changing situations and solve hard problems.  I can be a valuable asset to your company as a hard working problem solver, and I would love to still cook for the office on occasion!",
+    "bio2": "I live in Austin, and I graduated from the University of Texas bootcamp for full-stack web development and software engineering. I am a focused and quick-learning developer,experienced in both front end and back end construction, computer science, programming, and UX design for a multitude of projects. Great chef, but even better at JavaScript."
 };
